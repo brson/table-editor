bb0479004542250d3e5a0e65406b972b6f60269e
Brian Anderson <andersrb@gmail.com>
2021-08-07 12:21:21 -0600

    .

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
 #[macro_use] extern crate rocket;
 
+use serde_json::json;
+use rocket::fs::FileServer;
+use rocket_dyn_templates::Template;
+
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> Template {
+    Template::render("index", json!({}))
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/", routes![index])
+        .mount("/static", FileServer::from("static"))
+        .attach(Template::fairing())
 }
