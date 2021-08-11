70c6dd897eb76eb555d325e86410a2de9fd4be25
Brian Anderson <andersrb@gmail.com>
2021-08-11 11:54:22 -0600

    .

--- static/script.js
+++ static/script.js
@@ -1,22 +1,36 @@
 console.assert(jspreadsheet);
 
 const tableElement = document.getElementById("table");
+console.assert(tableElement);
+const saveButtonElement = document.getElementById("save-button");
+console.assert(saveButtonElement);
 
-function documentNameFromPageUrl() {
+async function run() {
+    const fileName = fileNameFromPageUrl();
+
+    if (fileName.endsWith(".csv")) {
+        await tableMode(fileName);
+    } else {
+        console.log("todo");
+    }
+}
+
+function fileNameFromPageUrl() {
     const url = new URL(document.URL);
     const path = url.pathname;
     console.assert(path[0] == '/');
     return path.slice(1);
 }
 
-async function run() {
-    const file = documentNameFromPageUrl();
+async function tableMode(fileName) {
+    saveButtonElement.addEventListener(
+        "click", async () => await onSaveButtonClick(fileName));
 
-    if (file.endsWith(".csv")) {
-        await loadFile(file);
-    } else {
-        console.log("todo");
-    }
+    await loadFile(fileName);
+}
+
+async function onSaveButtonClick(fileName) {
+    console.log("todo");
 }
 
 async function loadFile(fileName) {
